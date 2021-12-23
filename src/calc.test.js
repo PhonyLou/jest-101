d33a0941f1f22e9e48f95d77d0193f0db21fb7ae
Qilin LOU <1154537787@qq.com>
2021-12-23 23:15:18 +0800

    toEqual & toBe would be same on primitives

--- src/calc.test.js
+++ src/calc.test.js
@@ -4,4 +4,12 @@ describe('calculator', () => {
     it('adds two numbers', () => {
         expect(add(1, 2)).toEqual(3)
     })
+
+    it('basic usage', () => {
+        expect(1+1).toEqual(2)
+        expect(1+1).toBe(2)
+        
+        expect('Hello Qilin').toEqual('Hello Qilin')
+        expect('Hello Qilin').toBe('Hello Qilin')
+    })
 })

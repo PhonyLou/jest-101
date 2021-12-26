ad813a49f3a731fefd66127aaeb3396d5c5a016b
PhonyLou <1154537787@qq.com>
2021-12-26 23:10:22 +0800

    finish addition function via triangulation, prove function from different cases

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,4 +1,4 @@
-const add = (a, b) => 2 + b
+const add = (a, b) => a + b
 
 describe('addition', () => {
     it('returns 5 when adding 2 and 3', () => {
@@ -11,5 +11,11 @@ describe('addition', () => {
         const a = 2, b = 4
         const result = add(a, b)
         expect(result).toEqual(6)
+    })
+
+    it('returns 7 when adding 3 and 4', () => {
+        const a = 3, b = 4
+        const result = add(a, b)
+        expect(result).toEqual(7)
     })
 })

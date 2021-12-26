500bdb996f5fe017844deb646d1962485b20cf2b
PhonyLou <1154537787@qq.com>
2021-12-26 22:51:07 +0800

    add mock & stub in test

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,2 @@
-// function add(x, y) {
-//     return x + y;
-// }
-
-// module.exports = add
-
 const add = (x, y) => x + y
 export {add}
--- src/calc.test.js
+++ src/calc.test.js
@@ -33,4 +33,32 @@ describe('calculator', () => {
         const confirmedUserSet = expect.arrayContaining(['Qilin', 'Juntao'])
         expect(users).toEqual(confirmedUserSet)
     })
+
+    it('mock implementation', () => {
+        const fakeAdd = jest.fn().mockImplementation((a, b) => 5)
+        expect(fakeAdd(1, 1)).toBe(5)
+        expect(fakeAdd).toHaveBeenCalledWith(1, 1)
+
+    })
+})
+
+export const fetchUser = (id, process) => {
+    return fetch(`http://localhost:4000/users/${id}`)
+}
+
+describe('mock API call', () => {
+    const user = {
+        name: 'Qilin'
+    }
+    it('mock fetch', () => {
+        // given
+        global.fetch = jest.fn().mockImplementation(() => Promise.resolve({user}))
+        const process = jest.fn()
+
+        // when
+        fetchUser(111).then(x => console.log(x))
+
+        // then
+        expect(global.fetch).toHaveBeenCalledWith('http://localhost:4000/users/111')
+    })
 })
